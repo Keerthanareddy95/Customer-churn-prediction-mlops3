94880004c06a96f9ef230153d869e6c905885566
Keerthanareddy95 <keerthanareddy95k@gmail.com>
2024-06-09 10:32:08 +0530

    added training pipeline

---
+++ pipelines/training_pipeline.py
@@ -0,0 +1,19 @@
+from zenml import pipeline
+from steps.ingest_data import ingest_df
+from steps.clean_data import clean_df
+from steps.model_train import train_model
+from steps.evaluation import evaluate_model
+
+@pipeline
+def train_pipeline(data_path: str):
+    """
+    Args:
+        data_path: str
+    Returns:
+        mse: float
+        rmse: float
+    """
+    df = ingest_df(data_path)
+    clean_df(df)
+    train_model(df)
+    evaluate_model(df)
